3479895c4d67209e2dd04a44bff04e8b9972243e
Raymond Ottun <147501409+raysca@users.noreply.github.com>
2026-02-15 17:17:55 +0000

    chore: cleanup

--- app/playwright.config.ts
+++ app/playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, devices } from '@playwright/test'
+import {defineConfig, devices} from '@playwright/test'
 
 export default defineConfig({
     testDir: './tests/e2e',
@@ -17,15 +17,15 @@ export default defineConfig({
     projects: [
         {
             name: 'chromium',
-            use: { ...devices['Desktop Chrome'] },
+            use: {...devices['Desktop Chrome']},
         },
         {
             name: 'firefox',
-            use: { ...devices['Desktop Firefox'] },
+            use: {...devices['Desktop Firefox']},
         },
         {
             name: 'webkit',
-            use: { ...devices['Desktop Safari'] },
+            use: {...devices['Desktop Safari']},
         },
     ],
 
